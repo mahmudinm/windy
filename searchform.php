ff68726a437af5b0e369e3395f60f779f444678f
mahmudin muttaqin <claxmanx@gmail.com>
2018-10-12 14:03:29 +0700

    styling form search

--- searchform.php
+++ searchform.php
@@ -1,13 +1,18 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 
 	<div class="input-group mb-3">
-	  <input type="text" class="form-control search-field" style="padding: 10px;" 
+
+	  <input type="text" class="form-control search-field" 
 	      placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>"
 	            value="<?php echo get_search_query() ?>" name="s"
 	            title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>">		
+
 	  <div class="input-group-append">
-	    <button class="btn btn-outline-secondary" type="button"><span class="fa fa-search"></span></button>
+
+	    <button class="btn btn-outline-secondary btn-search" type="button"><span class="fa fa-search"></span></button>
+
 	  </div>
+
 	</div>
 
 </form> 
