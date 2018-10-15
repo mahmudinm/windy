a2e9c61b651b7de7a4829752705b1c4fab647d61
mahmudin muttaqin <claxmanx@gmail.com>
2018-10-15 12:33:46 +0700

    edit full width page

--- page-templates/full-width-page.php
+++ page-templates/full-width-page.php
@@ -5,13 +5,27 @@ Template Name: Full Width
 get_header();
 ?>
 	<?php if (have_posts()): ?>
-
+		
 		<?php if (has_post_thumbnail()): ?>
+			
+			</div><!-- End container -->
 
-			<?php the_post_thumbnail( 'large', ['class' => 'thumbnail-page'] ); ?>
+			<div style="background: url(<?= get_the_post_thumbnail_url( null, 'large' ); ?>) no-repeat;background-size: cover;" class="hero-page">
+
+				<?php while (have_posts()): the_post(); ?>
+						
+						<!-- <?= get_the_post_thumbnail_url( null, 'large' ); ?> -->
+
+						<h2 class="text-center hero-page__heading"><?php the_title(); ?></h2>
+
+				<?php endwhile ?>
+
+			</div>
+			
+			<div class="container">
 			
 		<?php endif ?>
-		
+
 		<div class="starter-template">
 
 			<div class="row">
@@ -20,7 +34,7 @@ get_header();
 
 					<?php while (have_posts()): the_post(); ?>
 							
-								<?php get_template_part( 'template-parts/content', 'page' ); ?>				
+							<?php get_template_part( 'template-parts/content', 'page' ); ?>				
 
 					<?php endwhile ?>
 
